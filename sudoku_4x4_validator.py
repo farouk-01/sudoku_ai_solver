9961ba3c0ef4f1c990a987dd4051d23c2b007ad3
farouk_01 <farouk_01@outlook.com>
2026-01-01 23:20:23 -0500

    Added missing unique block rule

--- sudoku_4x4_validator.py
+++ sudoku_4x4_validator.py
@@ -1,5 +1,16 @@
 import numpy as np
+S_D = {1, 2, 3, 4}
+
+def valid_cell(cell):
+    return cell in S_D
 
 def valid_row(row):
-    return len(row) == len(set(row))
+    return set(row) == S_D
+
+def valid_col(col):
+    return set(col) == S_D
+
+def valid_block(block):
+    return set(block) == S_D
+
     
